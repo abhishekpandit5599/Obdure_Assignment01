0ac7136f13b1fbb02b828229e6d390b9feab933f
Abhishek Sharma <abhishekpandit5599@gmail.com>
2022-03-27 00:29:08 +0530

    third

---
+++ Program_03.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int reverse(int);
+
+int reverse(int n){
+    int rem,result=0;
+    while (n>0){
+        rem = n%10;
+        n /= 10;
+        result = result *10 +rem;
+    }
+    return result;
+}
+
+void main(){
+    int n;
+    printf("Enter the value of N : ");
+    scanf("%d",&n);
+    printf("Reverse Digit %d \n",reverse(n));
+}
